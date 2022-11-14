d14de9e75413762168875da3cbe0a0c242badd07
007jag <114781974+007jag@users.noreply.github.com>
2022-11-14 16:49:54 -0500

    added game.js and game.css file with working quistions and answers

---
+++ game.js
@@ -0,0 +1,79 @@
+const question = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName('choice-text'));
+
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuesions = [];
+
+let questions = [
+    {
+        question: 'What is generally considered to be the first "pony car??',
+        choice1: 'Ford Mustang',
+        choice2: 'Mercury Cougar',
+        choice3: 'Chevrolet Camero',
+        choice4: 'Pontiac Firebird',
+        answer: 1,
+    },
+    {
+        question:
+            "What was the first Japanese car to be produced in the United States?",
+        choice1: "Toyota Camry",
+        choice2: "Nissan Maxima",
+        choice3: "Honda Accord",
+        choice4: "Mazada Miata",
+        answer: 3,
+    },
+    {
+        question: " What car sold more than one million units in 1965, setting a record that still stands today",
+        choice1: "Pontiac GTO",
+        choice2: "Buick WildCat",
+        choice3: "Ford ThunderBird",
+        choice4: "Chevrolet Implala",
+        answer: 4,
+    },
+];
+
+//CONSTANTS
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuesions = [...questions];
+    getNewQuestion();
+};
+
+getNewQuestion = () => {
+    if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        //go to the end page
+        return window.location.assign('/end.html');
+    }
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+    currentQuestion = availableQuesions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach((choice) => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    });
+
+    availableQuesions.splice(questionIndex, 1);
+    acceptingAnswers = true;
+};
+
+choices.forEach((choice) => {
+    choice.addEventListener('click', (e) => {
+        if (!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
+        getNewQuestion();
+    });
+});
+
+startGame();
