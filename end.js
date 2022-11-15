195845824d9eb0fab1a62373f374eb2a8b6d09bc
007jag <114781974+007jag@users.noreply.github.com>
2022-11-15 00:34:32 -0500

    fixed score updating bug

--- end.js
+++ end.js
@@ -1,7 +1,7 @@
 const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
-const mostRecentScore = localStorage.getItem('mostRecentStore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener("keyup", () => {
